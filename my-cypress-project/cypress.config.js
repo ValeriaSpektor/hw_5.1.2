0e35aba37bd0db81ddd71b231ee5672a217eda7b
Valeria Spektor <vlapin84@gmail.com>
2024-03-16 11:01:23 +0200

    added id

--- my-cypress-project/cypress.config.js
+++ my-cypress-project/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "cx3d3q",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
